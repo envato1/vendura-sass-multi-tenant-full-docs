cf56aba54fbd14e209c0e1bcf100d352709ca269
mgcodeur <mgcodeur@gmail.com>
2025-09-23 18:57:26 +0300

    feat: enable preview mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,12 @@ export default defineNuxtConfig({
   },
   content: {
     preview: {
-      api: 'https://api.nuxt.studio'
+      api: 'https://api.nuxt.studio',
+      gitInfo: {
+        name: 'vendura-sass-multi-tenant-full-docs',
+        owner: 'envato1',
+        url: 'https://github.com/envato1/vendura-sass-multi-tenant-full-docs'
+      }
     }
   },
   routeRules: {
